b9a4575b59a2f3ceccf4b89d0c6f44a3524c23f4
roman.karanchuk <romankaranchuk@gmail.com>
2025-08-02 19:32:35 +0200

    task4: implement ChatRepositoryTest

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,24 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
 
     fun getReplyMessage(): Flow<String> {
+        var currentDelay = 1000L
         return api.getReply()
+            .retryWhen { _, _ ->
+                delay(currentDelay)
+                currentDelay *= DELAY_FACTOR
+                true
+            }
+    }
+
+    companion object {
+        private const val DELAY_FACTOR = 2
     }
 }
